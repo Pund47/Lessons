341c349b5af4d6e36cd6a44437c0a8f47e39bdf3
Pund47 <ilin_yuriy@mail.ru>
2024-06-28 09:48:11 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+#1rd program
+print((9**0.5)*5)
+#2rd program
+print(9.99 >9.98 and 1000!=1000.1)
+#3rd program
+print( (1234% 1000)//10 + (5678% 1000)//10)
+#4th program
+print(int(13.42) == int((42.13*100)% 100) or int(42.13)== int((12.42*100)%100))
