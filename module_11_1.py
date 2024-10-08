e938194486a60f26536852e6e6923eeba38072d3
Pund47 <ilin_yuriy@mail.ru>
2024-10-08 13:32:44 +0300

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Чтение данных из Excel файла
+data = pd.read_excel('data.xlsx')
+# Вывод первых нескольких строк датафрейма
+print(data.head())
+
+# Создание датафрейма
+data = pd.DataFrame({
+    'name': ['Alice', 'Bob', 'Charlie'],
+    'age': [25, 30, 22]
+})
+# Запись в Exсel файл
+data.to_excel('output.xlsx', index='name')
+
+df = pd.DataFrame(data,index=['Alice'])
+print(df)
