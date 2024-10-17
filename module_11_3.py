82aa9b838711efa5627ad6ce5bc846eea97776fd
Pund47 <ilin_yuriy@mail.ru>
2024-10-17 08:01:09 +0300

    Update module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -1,14 +1,16 @@
+import inspect
 import sys
 from pprint import pprint
 
 def introspection_info(obj):
-    list = {}
-    list['type'] = type(obj)
-    list['atribute'] = dir(obj)
-
-    list['place']  = sys.path
-    #list.append(
-    return list
+list = {}
+list['type'] = type(obj)
+list['atribute'] = dir(obj)
+list['module'] = inspect.getmodule(obj)
+list['metods'] = inspect.getmembers(obj)
+list['module_of_call'] = obj.__ne__
+list['place'] = sys.path
+return list
 
 number_info = introspection_info(42)
 pprint(number_info)
