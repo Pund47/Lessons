45a273bdc5e0cfb8db5fc8b6fe169ddae0de5959
Pund47 <ilin_yuriy@mail.ru>
2024-10-21 12:46:02 +0300

    Update module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -3,14 +3,27 @@ import sys
 from pprint import pprint
 
 def introspection_info(obj):
-list = {}
-list['type'] = type(obj)
-list['atribute'] = dir(obj)
-list['module'] = inspect.getmodule(obj)
-list['metods'] = inspect.getmembers(obj)
-list['module_of_call'] = obj.__ne__
-list['place'] = sys.path
-return list
+    my_list = {}
+    list_callable = []
+    list_no_callable = []
+    my_list['type'] = type(obj)
+    all_met = dir(obj)
+    for i in all_met:
+        if callable(getattr(obj,i)) == True:
+            list_callable.append(i)
+        else:
+            list_no_callable.append(i)
+    my_list['atribute'] = list_no_callable #не вызываемый
+    my_list['metods'] = list_callable  #вызываемый
+    my_list['module'] = inspect.getmodule(obj)
+    my_list['module_of_call'] = getattr(obj,"__module__","Не имеет модуль.")
+
+    try:
+        my_list['place'] = inspect.getfile(obj)
+    except TypeError:
+        my_list['place'] = "Файл не определен или объект встроенный"
+
+    return my_list
 
 number_info = introspection_info(42)
 pprint(number_info)
