30827f057f4c4e83c6c1c56da16c6428de626776
Pund47 <ilin_yuriy@mail.ru>
2024-11-04 13:40:59 +0300

    Add files via upload

---
+++ module_12_1.py
@@ -0,0 +1,31 @@
+import runner
+import unittest
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        gulyaka = runner.Runner("gulyaka")
+        for i in range (10):
+            gulyaka.walk()
+        self.assertEqual(gulyaka.distance,50)
+            
+
+    def test_run(self):
+        runer = runner.Runner("runer")
+        for i in range (10):
+            runer.run()
+        self.assertEqual(runer.distance, 100)
+
+    def test_challenge (self):
+        gulyaka = runner.Runner("gulyaka")
+        runer = runner.Runner("runer")
+        for i in range (10):
+            runer.run()
+            gulyaka.walk()
+        self.assertNotEqual(gulyaka.distance,runer.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
