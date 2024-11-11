188ebf6e2e6fd5a99d16b0e0dc86fe4802f518cd
Pund47 <ilin_yuriy@mail.ru>
2024-11-11 18:38:27 +0300

    Update tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -14,17 +14,17 @@ class TournamentTest(unittest.TestCase):
 
 
     def setUp(self):
-        runer = runner_and_tournament.Runner("Усэйн")
-        runer.speed = 10
-        runer1 = runner_and_tournament.Runner("Андрей")
-        runer1.speed = 9
-        runer2 = runner_and_tournament.Runner("Ник")
-        runer2.speed = 3
+        self.runer = runner_and_tournament.Runner("Усэйн")
+        self.runer.speed = 10
+        self.runer1 = runner_and_tournament.Runner("Андрей")
+        self.runer1.speed = 9
+        self.runer2 = runner_and_tournament.Runner("Ник")
+        self.runer2.speed = 3
 
-    def test_Tournament(self, runer,runer1,runer2):
-        zabeg1 = runner_and_tournament.Tournament(90,[runer,runer2])
+    def test_Tournament(self):
+        zabeg1 = runner_and_tournament.Tournament(90,self.runer,self.runer2)
         self.all_results =  zabeg1.start()
-        zabeg2 = runner_and_tournament.Tournament(90, [runer1, runer2])
+        zabeg2 = runner_and_tournament.Tournament(90, self.runer1, self.runer2)
         self.all_results = zabeg2.start()
-        zabeg3 = runner_and_tournament.Tournament(90, [runer,runer1, runer2])
+        zabeg3 = runner_and_tournament.Tournament(90, self.runer,self.runer1, self.runer2)
         self.all_results = zabeg3.start()
