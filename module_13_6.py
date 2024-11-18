a4cf3881e931b301ce65cf128943fe6affe9c10d
Pund47 <ilin_yuriy@mail.ru>
2024-11-18 14:57:32 +0300

    Add files via upload

---
+++ module_13_6.py
@@ -0,0 +1,106 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+
+api = ""
+
+bot = Bot(token=api)
+dp = Dispatcher(bot,storage= MemoryStorage())
+
+kb = ReplyKeyboardMarkup()
+button = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+
+kb.add(button,button2)
+
+
+kb2 = InlineKeyboardMarkup()
+but1 = InlineKeyboardButton(text='Рассчитать норму калорий' , callback_data='calories')
+but2 = InlineKeyboardButton(text='Формулы расчёта' , callback_data='formulas')
+
+kb2.add(but1,but2)
+
+start_menu =ReplyKeyboardMarkup()
+
+
+
+class UserState(StatesGroup):
+    age    = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def starter(message):
+    await message.answer('Бу',reply_markup=kb2)
+
+@dp.callback_query_handler(text = 'calories')    #запуск расчётов!
+async def strt_of_calc(call):
+    message = call.message
+    await set_growth(message)
+
+@dp.callback_query_handler(text = 'formulas')    #запуск формул!
+async def infor(call):
+    await call.message.answer('https://www.calc.ru/Formula-Mifflinasan-Zheora.html')
+    await call.answer()
+
+
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await  message.answer('Выберите действие:',reply_markup= kb)
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer('Информация о боте, нетути)))')
+
+@dp.message_handler(text='Рассчитать')
+async def nachalo_rascheta(message):
+    await set_growth(message)
+
+
+
+
+@dp.message_handler(text='Calories')
+async def set_growth(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def fsm_handler(message,state):
+    await state.update_data(age = message.text)
+    date = await state.get_data()
+
+    await message.answer(f"Введите свой рост:")
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def fsm_handler(message,state):
+    await state.update_data(growth = message.text)
+    date = await state.get_data()
+
+    await message.answer(f"Введите свой вес:")
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def fsm_handler(message,state):
+    await state.update_data(weight = message.text)
+    date = await state.get_data()
+    await send_calories(message,date,state)
+
+@dp.message_handler()
+async def  send_calories(message,date,state):
+    callories = int(10*int(date["weight"]) + 6.25 * int(date["growth"]) + 5 * int(date["age"]))
+    await message.answer(f"Необходимое количество килокалорий (ккал) в сутки: {callories}")
+    await state.finish()
+
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
