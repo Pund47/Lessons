0dce874e23283d3f49e485858621483326424f86
Pund47 <ilin_yuriy@mail.ru>
2024-11-21 15:32:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+API = "7451720096:AAGbEXhGq4mAIpn57YZzFemmGUJdhZb9Fuw"
+
+opisanie1 = "Зелёный дракон"
+opisanie2 = "Дракон с платочком"
+opisanie3 = "Забавный дракон"
+opisanie4 = "Прикольный дрэгон"
