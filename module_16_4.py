538691400a4b2020c755ef8a6641367a6574b296
Pund47 <ilin_yuriy@mail.ru>
2024-12-05 14:36:32 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -17,17 +17,21 @@ async def root1()-> List[User]:
     return users
 
 @app.post('/user/{username}/{age}')
-async def add_new_user(user:User, username,age) -> str:
-   current_index = 0
+async def add_new_user(user:User, username:str,age:int) -> str:
    if len(users)== 0:
        current_index = 1
    else:
-       current_index = (len(users) + 1)
+       max_id = 0
+       for i in range(1,len(users)):
+           if int(users[i].id) > max_id:
+               max_id = int(users[i].id)
+       current_index = int(max_id + 1)
    User.id = current_index
    User.username = username
    User.age = age
+   #users[current_index] = (username, age)
    users.append(User)
-   return (f"User {current_index} is registered")
+   return (f"User {User} is registered")
 
 #put запрос по маршруту '/user/{user_id}/{username}/{age}'
 @app.put('/user/{user_id}/{username}/{age}')
@@ -44,7 +48,8 @@ async def update_user(user:User,user_id:int,username:str,age:int)-> str:
 @app.delete('/user/{user_id}')
 async def del_user(user_id:int)-> str:
     try:
+        userForDel = users[user_id]
         users.pop(user_id)
-        return (f"Пользователь {user_id} был удалён!")
+        return (f"Пользователь {userForDel} был удалён!")
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
